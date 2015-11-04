54d8d7b50e8eba467636c7034bef9a1af1e627a1
abureau <abureau6@gmail.com>
2015-11-04 12:00:20 -0500

    retrait de 2 accolades

--- src/UnphasedAnalysisOutput.cpp
+++ src/UnphasedAnalysisOutput.cpp
@@ -120,7 +120,7 @@ void UnphasedAnalysis::outputTabularHeaders(UnphasedOptions &options) {
       }
     }
 }
-}    
+    
 }
 
 void UnphasedAnalysis::outputTabular(UnphasedOptions &options, vector<int>& combination, double null, double alternative, int df) {
@@ -441,7 +441,7 @@ void UnphasedAnalysis::outputTabular(UnphasedOptions &options, vector<int>& comb
                     }
                 }
             }
-	        }
+	    //    }
 }
 
 inline double round(double x, double y) {
