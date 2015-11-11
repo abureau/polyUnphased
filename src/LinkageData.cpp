8618f8f5f605ce642eb7225097e4b02a00f0b0b7
abureau <abureau6@gmail.com>
2015-11-11 10:22:07 -0500

    Essai de correction de redimensionnement de vecteur

--- src/LinkageData.cpp
+++ src/LinkageData.cpp
@@ -50,7 +50,8 @@ void LinkageData::readpedfile(string &filename) {
     }
     *outStream << "Reading pedigree file " << filename << "..." << flush;
 
-	Kvec.resize(nlocus, 2);
+	Kvec.resize(nlocus);
+	Kvec = 2;
     string line;
     while ((line = getline(infile)) != "") {
         Subject subject;
