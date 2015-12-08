f69a2060f1f53bdc7973fe15b15a9a892d4338a7
abureau <abureau6@gmail.com>
2015-12-08 13:37:13 -0500

    Ultime correction à l'affichage

--- src/UnphasedAnalysisOutput.cpp
+++ src/UnphasedAnalysisOutput.cpp
@@ -915,8 +915,8 @@ void UnphasedAnalysis::outputResults(vector<int> &combination, string &trait,
     if (typeOfPhenotype == "polytomous") {
     *outStream << setw(haplotypeWidth) << " ";
     for (int k = 1; k < K; k++)
-    	if (options.individual) *outStream << "Level " << setw(56) << k  ;
-    	else *outStream << "Level " << setw(32) << k ;
+    	if (options.individual) *outStream << "Level " << setw(54) << k  ;
+    	else *outStream << "Level " << setw(30) << k ;
     *outStream << endl;
     	}
     *outStream << setw(haplotypeWidth) << (options.genotype ? "Genotype" : (alleles ? "Allele" : "Haplotype"));
